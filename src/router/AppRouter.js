85654bfbb256fa98ae7e5341e30b595bc8a80fa5
zhurunkai <1044180570@qq.com>
2020-10-28 17:39:07 +0800

    revise

---
+++ src/router/AppRouter.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Home from '../pages/home'
+
+class AppRouter extends Component {
+    render() {
+        return (
+            <Home></Home>
+        )
+    }
+}
+
+export default AppRouter;
