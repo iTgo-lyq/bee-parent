6d4ccc6e2bf3eb9d16e55e7c171a68fbe86fc9d7
kaik996 <1044180570@qq.com>
2020-11-02 23:47:09 +0800

    创建文件

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,7 +2,7 @@ const defaultState = {
     inputValue: 'haha'
 }
 export default (state = defaultState, action) => {
-    if(action.type==='change')
+    // if(action.type==='change')
         
     return state
 }
